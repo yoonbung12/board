81ae5f5bdc163e79ae9f872d8f373778e9e899c6
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2022-12-26 16:41:17 +0900

    mapper,boardVO

--- board/src/main/java/com/board/domain/BoardVO.java
+++ board/src/main/java/com/board/domain/BoardVO.java
@@ -20,6 +20,42 @@ public class BoardVO {
 	private String writer;
 	private Date regdate;
 	private int viewcnt;
+	public int getIdx() {
+		return idx;
+	}
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public Date getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+	public int getViewcnt() {
+		return viewcnt;
+	}
+	public void setViewcnt(int viewcnt) {
+		this.viewcnt = viewcnt;
+	}
 	
 	
 }
