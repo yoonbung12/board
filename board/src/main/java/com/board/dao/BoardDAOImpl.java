b24a2b74a35723f346887624b264287107f1f099
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2023-01-02 18:25:34 +0900

    게시물 수정 dao 추가

--- board/src/main/java/com/board/dao/BoardDAOImpl.java
+++ board/src/main/java/com/board/dao/BoardDAOImpl.java
@@ -50,6 +50,16 @@ public class BoardDAOImpl implements BoardDAO {
 		
 		return sql.selectOne(namespace + ".view", bno);
 	}
+
+	//게시물 수정
+	@Override
+	public void modify(BoardVO vo) throws Exception {
+		
+		sql.update(namespace + ".modify" , vo);
+		
+	}
+	
+	
 	
 
 	
