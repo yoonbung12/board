b981a8b76cc9401697c57c8c82b3a46086319edc
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2023-01-02 16:52:56 +0900

    컨트롤러 추가

--- board/src/main/java/com/board/controller/BoardController.java
+++ board/src/main/java/com/board/controller/BoardController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.board.domain.BoardVO;
 import com.board.service.BoardService;
@@ -51,7 +52,15 @@ public class BoardController {
 		
 	//게시물 조회
 	@RequestMapping(value="/view", method = RequestMethod.GET)
-	public void getView() throws Exception {
+	public void getView(
+			
+				@RequestParam("bno") int bno,
+				Model model
+			
+			
+			) throws Exception {
+		
+		BoardVO vo = service.view(bno);
 		
 	}
 
