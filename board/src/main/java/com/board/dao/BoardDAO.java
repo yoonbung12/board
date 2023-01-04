a5b7cd99d07c6d52dbcbc831621d898b0c248853
yoonbung12 <84695780+yoonbung12@users.noreply.github.com>
2023-01-04 18:12:20 +0900

    페이징 하기

--- board/src/main/java/com/board/dao/BoardDAO.java
+++ board/src/main/java/com/board/dao/BoardDAO.java
@@ -23,5 +23,7 @@ public interface BoardDAO {
 	//게시물 총 갯수
 	public int count() throws Exception;
 	
+	//게시물 목록 + 페이징
+	public List<BoardVO> listPage(int displayPost, int postNum) throws Exception;
 	
 }
--- board/src/main/java/com/board/dao/BoardDAOImpl.java
+++ board/src/main/java/com/board/dao/BoardDAOImpl.java
@@ -1,5 +1,6 @@
 package com.board.dao;
 
+import java.util.HashMap;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -66,11 +67,23 @@ public class BoardDAOImpl implements BoardDAO {
 		sql.delete(namespace + ".delete", bno);
 	}
 	
-	//게시물 총 갯수(페이징)
+	//게시물 총 갯수
 	@Override
 	public int count() throws Exception {
 
 		return sql.selectOne(namespace + ".count");
+	}
+
+	//게시물 목록 + 페이징
+	@Override
+	public List<BoardVO> listPage(int displayPost, int postNum) throws Exception {
+
+		HashMap<String, Integer> data = new HashMap<String, Integer>();
+		
+		data.put("displayPost", displayPost);
+		data.put("postNum", postNum);
+		
+		return sql.selectList(namespace + "list.page", data);
 	}
 	
 	
